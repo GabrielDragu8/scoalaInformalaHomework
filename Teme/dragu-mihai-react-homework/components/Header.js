de125fa2f2363f6898f53a2d0d6202708c3cccdf
GabrielDragu8 <147907671+GabrielDragu8@users.noreply.github.com>
2024-03-10 21:55:54 +0200

    Delete Teme/dragu-mihai-react-homework directory

--- Teme/dragu-mihai-react-homework/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = ({text}) => {
-  return <div className='HeaderBar'>{text}</div>;
-};
-
-export default Header;
--- Teme/dragu-mihai-react-homework/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchBar = () => {
-  return <div className='SearchBar'>
-    <input type="text" id="search" className='inputSearch'></input>
-  </div>;
-};
-
-export default SearchBar;
